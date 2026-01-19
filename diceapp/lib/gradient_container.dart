6ccdeb17c6994245ab46e0e42b9d94586631c165
Raunak Kandoi <raunak.kandoi@iwedia.com>
2026-01-19 13:14:27 +0530

    [DiceApp] Create a custom widget

--- diceapp/lib/gradient_container.dart
+++ diceapp/lib/gradient_container.dart
@@ -1,30 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:diceapp/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
 
   const GradientContainer({super.key});
 
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
         gradient: LinearGradient(
           colors: [
             Color.fromARGB(255, 182, 8, 159),
-            Color.fromARGB(255, 39, 152, 204),
+            Color.fromARGB(255, 235, 154, 192),
           ],
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
         ),
       ),
       child: const Center(
-        child: Text(
-          'Hello, World!',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
+        child: StyledText(),
       ),
     );
   }
