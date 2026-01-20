209c21e09dccf1b052dd818bd5a89d8718d2860a
Raunak Kandoi <raunak.kandoi@iwedia.com>
2026-01-20 14:19:05 +0530

    [DiceApp] Styling Button's and Working with Padding

--- diceapp/lib/gradient_container.dart
+++ diceapp/lib/gradient_container.dart
@@ -68,22 +68,54 @@ class GradientContainer extends StatelessWidget {
       ),
       child: Center(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             Image.asset(
               'assets/images/dice-1.png',
               width: 200,
             ),
-            const ElevatedButton(
+            const SizedBox(height: 20),
+            ElevatedButton(
               onPressed: rollDice,
-              child: Text('Roll Dice'),
+              style: TextButton.styleFrom(
+                // padding: const EdgeInsets.only(
+                //   top: 20,
+                // ),
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.black,
+                textStyle: const TextStyle(
+                  fontSize: 28,
+                ),
+              ),
+              child: const Text('Roll Dice'),
             ),
-            const OutlinedButton(
+            const SizedBox(height: 20),
+            OutlinedButton(
               onPressed: rollDice,
-              child: Text('Roll Dice'),
+              style: TextButton.styleFrom(
+                // padding: const EdgeInsets.only(
+                //   top: 20,
+                // ),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 28,
+                ),
+              ),
+              child: const Text('Roll Dice'),
             ),
-            const TextButton(
+            const SizedBox(height: 20),
+            TextButton(
               onPressed: rollDice,
-              child: Text('Roll Dice'),
+              style: TextButton.styleFrom(
+                // padding: const EdgeInsets.only(
+                //   top: 20,
+                // ),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 28,
+                ),
+              ),
+              child: const Text('Roll Dice'),
             ),
           ],
         ),
