feecd92b90f0b5681f84af0ebb3800bc3da40d1f
Raunak Kandoi <raunak.kandoi@iwedia.com>
2026-01-21 15:34:42 +0530

    [Quiz] Adding and passing functions as Values.

--- quiz/lib/start_screen.dart
+++ quiz/lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(BuildContext context) {
@@ -31,9 +33,7 @@ class StartScreen extends StatelessWidget {
           ),
           const SizedBox(height: 20),
           OutlinedButton.icon(
-            onPressed: () {
-              // when the button is pressed
-            },
+            onPressed: startQuiz,
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
               side: const BorderSide(
