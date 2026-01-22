ee000a02b3b054f79c7c2c3f146f3012d77cf241
Raunak Kandoi <raunak.kandoi@iwedia.com>
2026-01-22 11:58:38 +0530

    [Quiz] Configuring Column for Question screen

--- quiz/lib/questions_screen.dart
+++ quiz/lib/questions_screen.dart
@@ -12,8 +12,34 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'Questions Screen',
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Questions Screen'),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 2'),
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 3'),
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 4'),
+          ),
+        ],
+      ),
     );
   }
 }
