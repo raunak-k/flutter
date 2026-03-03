bc1d66671f1390f1ebf13eac041b07aa060a1077
Raunak Kandoi <raunak.kandoi@iwedia.com>
2026-03-03 22:24:42 +0530

    [Meals] Adding Tab-based Navigation

--- meals_app/lib/screens/meals.dart
+++ meals_app/lib/screens/meals.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/meals_details.dart';
 import 'package:meals_app/widgets/meal_item.dart';
@@ -7,11 +7,11 @@ import 'package:meals_app/widgets/meal_item.dart';
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
     super.key,
-    required this.title,
+    this.title,
     required this.meals,
   });
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
 
   void selectMeal(BuildContext context, Meal meal) {
@@ -61,9 +61,13 @@ class MealsScreen extends StatelessWidget {
       );
     }
 
+    if (title == null) {
+      return content;
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(title!),
       ),
       body: content,
     );
